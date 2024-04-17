5f6d36d73a783df29aaa67b418de33ee61bd87bb
JinukRyu <ryu@j1n.uk>
2024-04-17 14:54:50 +0900

    ㅎ

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import './App.css';
+import logo from "./images/logo_j1n.uk.png";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Box } from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1976d2",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <React.Fragment>
+      <ThemeProvider theme={darkTheme}>
+        <AppBar position="static" color="primary">
+          <Toolbar sx={{ justifyContent: "center" }}>
+            <Box
+              component="img"
+              sx={{
+                width: 120,
+              }}
+              alt="J1N.UK Logo"
+              src={logo}
+            />
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
+    </React.Fragment>
   );
 }
 
