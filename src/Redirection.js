cd26983e61ee5ad7d5b679626f635dc9cb3a93cb
JinukRyu <ryu@j1n.uk>
2024-04-18 10:08:55 +0900

    리디렉션

--- src/Redirection.js
+++ src/Redirection.js
@@ -1,11 +1,28 @@
-import { Typography } from "@mui/material";
 import * as React from "react";
+import { useLocation } from "react-router-dom";
 
 function Redirection() {
+  const location = useLocation();
+
+  // 현재 URL 경로
+  const currentPath = location.pathname.replace("/", "");
+
+  //   // 쿼리 매개변수
+  //   const queryParams = new URLSearchParams(location.search);
+
+  //   // 쿼리 매개변수 값 가져오기
+  //   const paramValue = queryParams.get("paramName");
+
+  window.location.href =
+    "https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=" +
+    currentPath;
+
+  // TODO: 통계?
+
   return (
-    <Typography variant="h10" color="">
-      Redirection
-    </Typography>
+    <div>
+      <p>Current Path: {currentPath}</p>
+    </div>
   );
 }
 
